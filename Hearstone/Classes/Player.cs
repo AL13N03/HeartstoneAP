656562758d220bf499cb2b1652391eed24cbd847
AL13N03 <raf.vanhoegaerden@gmail.com>
2022-03-07 13:10:20 +0100

    Add project files.

---
+++ Hearstone/Classes/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hearstone.Classes
+{
+    class Player
+    {
+    }
+}
